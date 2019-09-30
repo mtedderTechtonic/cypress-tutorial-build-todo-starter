890a26326db8c2b6a23cb0472e547336daddd220
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-30 14:40:41 -0600

    Creates handleChange method, adds baseUrl to cypress config

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,21 +1,19 @@
 describe('Input form', () => {
-	context('Visits the page where the application lives', () => {
-        beforeEach(() => {
-            cy.visit('http://localhost:3030')
-        })
-		it('Focuses input on load', () => {
-            
-            
-            cy.focused()
-                .should('have.class', 'new-todo')
-        })
-        
-        it.only('Accepts input', () => {
-            const typedText = 'Buy Coffee'
+	context('Adds todo to input field', () => {
+		beforeEach(() => {
+			cy.visit('/')
+		})
 
-            cy.get('.new-todo')
-            .type(typedText)
-            .should('have.value', typedText)
-        })
+		it('Focuses input on load', () => {
+			cy.focused().should('have.class', 'new-todo')
+		})
+
+		it('Accepts input', () => {
+			const typedText = 'Buy Coffee'
+
+			cy.get('.new-todo')
+				.type(typedText)
+				.should('have.value', typedText)
+		})
 	})
 })
