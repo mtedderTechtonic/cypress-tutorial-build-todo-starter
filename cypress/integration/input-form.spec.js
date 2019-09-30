3dc53a12bf19537756f5d915e5bbdb44b070e8ad
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-30 14:20:22 -0600

    Adds autoFocus to input component, tests pass for accepting input

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,7 +1,19 @@
 describe('Input form', () => {
 	context('Visits the page where the application lives', () => {
 		it('Focuses input on load', () => {
-			cy.visit('localhost:3030')
-		})
+            cy.visit('http://localhost:3030')
+            
+            cy.focused()
+                .should('have.class', 'new-todo')
+        })
+        
+        it.only('Accepts input', () => {
+            const typedText = 'Buy Coffee'
+            cy.visit('http://localhost:3030')
+
+            cy.get('.new-todo')
+            .type(typedText)
+            .should('have.value', typedText)
+        })
 	})
 })
