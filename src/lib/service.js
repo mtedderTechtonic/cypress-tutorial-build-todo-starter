4c59e810b9209939bc82afd104d5653320735251
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-10-01 11:12:23 -0600

    Adds code for toggling and debugging

--- src/lib/service.js
+++ src/lib/service.js
@@ -6,3 +6,5 @@ export const saveTodo = todo =>
 export const loadTodos = () => axios.get('http://localhost:3030/api/todos')
 
 export const destroyTodo = (id) => axios.delete(`http://localhost:3030/api/todos/${id}`)
+
+export const updateTodo = (todo) => axios.put(`http://localhost:3030/api/todos/${todo.id}`, todo)
